7baf8f0498a28e15ccda9a1295a490173b592826
노웅래 <wlroh.paul@gmail.com>
2023-01-14 18:29:39 +0900

    chore: 쓰레드 풀을 설정하라

---
+++ src/main/java/com/wlroh/async/ThreadPoolConfig.java
@@ -0,0 +1,18 @@
+package com.wlroh.async;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+@Configuration
+public class ThreadPoolConfig {
+
+    @Bean
+    public ThreadPoolTaskExecutor threadPoolTaskExecutor() {
+        ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
+        taskExecutor.setCorePoolSize(2); // 기본 스레드 수
+        taskExecutor.setMaxPoolSize(5); // 최대 스레드 수
+        taskExecutor.setThreadNamePrefix("Executor-");
+        return taskExecutor;
+    }
+}
