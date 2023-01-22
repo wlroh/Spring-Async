d1b7055ce9f3eca12432d73a92d63c080744e1b8
노웅래 <wlroh.paul@gmail.com>
2023-01-22 22:47:27 +0900

    test: 풀 사이즈 조회 메서드 를 수정하라

--- src/test/java/com/wlroh/async/threadtaskpoolexecutor/ThreadPoolStep.java
+++ src/test/java/com/wlroh/async/threadtaskpoolexecutor/ThreadPoolStep.java
@@ -3,6 +3,7 @@ package com.wlroh.async.threadtaskpoolexecutor;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 public class ThreadPoolStep {
 
@@ -23,7 +24,11 @@ public class ThreadPoolStep {
 
     public static void 사용중인_풀_사이즈_조회됨(final ThreadPoolTaskExecutor executor, final int poolSize) {
         final int currentPoolSize = executor.getPoolSize();
-        assertThat(currentPoolSize).isEqualTo(poolSize);
+        final int activeCount = executor.getActiveCount();
+        assertAll(
+                () -> assertThat(currentPoolSize).isEqualTo(poolSize),
+                () -> assertThat(activeCount).isEqualTo(poolSize)
+        );
     }
 
     public static void 현재_큐_사이즈_조회됨(final ThreadPoolTaskExecutor executor, final int queueSize) {
