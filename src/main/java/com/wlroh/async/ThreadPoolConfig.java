258a410ef9182be12259f6d20932f81f0af88829
노웅래 <wlroh.paul@gmail.com>
2023-02-10 01:51:41 +0900

    refactor: default executor를 SimpleAsyncTaskExecutor로 구성하도록 변경하라

--- src/main/java/com/wlroh/async/ThreadPoolConfig.java
+++ src/main/java/com/wlroh/async/ThreadPoolConfig.java
@@ -4,8 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
-import java.util.concurrent.ThreadPoolExecutor;
-
 @Configuration
 public class ThreadPoolConfig {
 
@@ -29,85 +27,6 @@ public class ThreadPoolConfig {
         taskExecutor.setThreadNamePrefix("Executor2-");
         taskExecutor.setWaitForTasksToCompleteOnShutdown(true);
         taskExecutor.setAwaitTerminationMillis(20_000L);
-        return taskExecutor;
-    }
-
-    @Bean
-    public ThreadPoolTaskExecutor threadPoolTaskExecutor3() {
-        final ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
-        taskExecutor.setCorePoolSize(1); // 기본 스레드 수
-        taskExecutor.setMaxPoolSize(1); // 최대 스레드 수
-        taskExecutor.setQueueCapacity(0);
-        taskExecutor.setThreadNamePrefix("Executor3-");
-        taskExecutor.setRejectedExecutionHandler(new ThreadPoolExecutor.AbortPolicy());
-        taskExecutor.setWaitForTasksToCompleteOnShutdown(true);
-        taskExecutor.setAwaitTerminationMillis(20_000L);
-        return taskExecutor;
-    }
-
-    @Bean
-    public ThreadPoolTaskExecutor threadPoolTaskExecutor4() {
-        final ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
-        taskExecutor.setCorePoolSize(1); // 기본 스레드 수
-        taskExecutor.setMaxPoolSize(1); // 최대 스레드 수
-        taskExecutor.setQueueCapacity(0);
-        taskExecutor.setThreadNamePrefix("Executor4-");
-        taskExecutor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
-        taskExecutor.setWaitForTasksToCompleteOnShutdown(true);
-        taskExecutor.setAwaitTerminationMillis(20_000L);
-        return taskExecutor;
-    }
-
-    @Bean
-    public ThreadPoolTaskExecutor threadPoolTaskExecutor5() {
-        final ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
-        taskExecutor.setCorePoolSize(1); // 기본 스레드 수
-        taskExecutor.setMaxPoolSize(1); // 최대 스레드 수
-        taskExecutor.setQueueCapacity(0);
-        taskExecutor.setThreadNamePrefix("Executor5-");
-        taskExecutor.setRejectedExecutionHandler(new ThreadPoolExecutor.DiscardPolicy());
-        taskExecutor.setWaitForTasksToCompleteOnShutdown(true);
-        taskExecutor.setAwaitTerminationMillis(20_000L);
-        return taskExecutor;
-    }
-
-    @Bean
-    public ThreadPoolTaskExecutor threadPoolTaskExecutor6() {
-        final ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
-        taskExecutor.setCorePoolSize(1); // 기본 스레드 수
-        taskExecutor.setMaxPoolSize(1); // 최대 스레드 수
-        taskExecutor.setQueueCapacity(2);
-        taskExecutor.setThreadNamePrefix("Executor6-");
-        taskExecutor.setRejectedExecutionHandler(new ThreadPoolExecutor.DiscardOldestPolicy());
-        taskExecutor.setWaitForTasksToCompleteOnShutdown(true);
-        taskExecutor.setAwaitTerminationMillis(20_000L);
-        return taskExecutor;
-    }
-
-    @Bean
-    public ThreadPoolTaskExecutor threadPoolTaskExecutor7() {
-        final ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
-        taskExecutor.setCorePoolSize(1); // 기본 스레드 수
-        taskExecutor.setMaxPoolSize(5); // 최대 스레드 수
-        taskExecutor.setQueueCapacity(0);
-        taskExecutor.setThreadNamePrefix("Executor7-");
-        taskExecutor.setWaitForTasksToCompleteOnShutdown(true);
-        taskExecutor.setAwaitTerminationMillis(20_000L);
-        taskExecutor.setKeepAliveSeconds(10);
-        return taskExecutor;
-    }
-
-    @Bean
-    public ThreadPoolTaskExecutor threadPoolTaskExecutor8() {
-        final ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
-        taskExecutor.setCorePoolSize(1); // 기본 스레드 수
-        taskExecutor.setMaxPoolSize(5); // 최대 스레드 수
-        taskExecutor.setQueueCapacity(0);
-        taskExecutor.setThreadNamePrefix("Executor8-");
-        taskExecutor.setWaitForTasksToCompleteOnShutdown(true);
-        taskExecutor.setAwaitTerminationMillis(20_000L);
-        taskExecutor.setKeepAliveSeconds(10);
-        taskExecutor.setAllowCoreThreadTimeOut(true);
         return taskExecutor;
     }
 }
--- src/main/java/com/wlroh/async/application/AsyncService.java
+++ src/main/java/com/wlroh/async/application/AsyncService.java
@@ -19,40 +19,4 @@ public class AsyncService {
         }
         log.info("end Test Method");
     }
-
-    @Async
-    public void asyncDefault2() {
-        // SimpleAsyncTaskExecutor 가 신규 Thread 생성하여 Task 수행
-        log.info("call Test Method");
-        try {
-            Thread.sleep(5_000L);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-        log.info("end Test Method");
-    }
-
-    @Async("threadPoolTaskExecutor4")
-    public void asyncWithThreadPoolTaskExecutor() {
-        // threadPoolTaskExecutor4 가 관리하는 Thread 이용해 Task 수행
-        log.info("call Test Method");
-        try {
-            Thread.sleep(5_000L);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-        log.info("end Test Method");
-    }
-
-    @Async("noBeanExecutor")
-    public void asyncNoBeanExecutor() {
-        // NoSuchBeanDefinitionException 예외 발생
-        log.info("call Test Method");
-        try {
-            Thread.sleep(5_000L);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-        log.info("end Test Method");
-    }
 }
