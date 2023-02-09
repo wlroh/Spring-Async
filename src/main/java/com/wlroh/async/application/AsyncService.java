331f38b17a3987228f9eabc665a8ad4c1f361cdd
노웅래 <wlroh.paul@gmail.com>
2023-02-10 01:49:18 +0900

    refactor: default executor를 bean으로 등록된 ThreadPoolTaskExecutor로 구성하도록 변경하라

--- src/main/java/com/wlroh/async/application/AsyncService.java
+++ src/main/java/com/wlroh/async/application/AsyncService.java
@@ -10,43 +10,7 @@ public class AsyncService {
 
     @Async
     public void asyncDefault() {
-        // SimpleAsyncTaskExecutor 가 신규 Thread 생성하여 Task 수행
-        log.info("call Test Method");
-        try {
-            Thread.sleep(5_000L);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-        log.info("end Test Method");
-    }
-
-    @Async
-    public void asyncDefault2() {
-        // SimpleAsyncTaskExecutor 가 신규 Thread 생성하여 Task 수행
-        log.info("call Test Method");
-        try {
-            Thread.sleep(5_000L);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-        log.info("end Test Method");
-    }
-
-    @Async("threadPoolTaskExecutor4")
-    public void asyncWithThreadPoolTaskExecutor() {
-        // threadPoolTaskExecutor4 가 관리하는 Thread 이용해 Task 수행
-        log.info("call Test Method");
-        try {
-            Thread.sleep(5_000L);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-        log.info("end Test Method");
-    }
-
-    @Async("noBeanExecutor")
-    public void asyncNoBeanExecutor() {
-        // NoSuchBeanDefinitionException 예외 발생
+        // myThreadPoolTaskExecutor 이름의 ThreadPoolTaskExecutor bean 이 관리하는 Thread 이용해 Task 수행
         log.info("call Test Method");
         try {
             Thread.sleep(5_000L);
--- src/main/java/com/wlroh/async/controller/AsyncController.java
+++ src/main/java/com/wlroh/async/controller/AsyncController.java
@@ -18,28 +18,4 @@ public class AsyncController {
         }
         return "ok";
     }
-
-    @GetMapping("/async-default2")
-    public String asyncDefault2() {
-        for (int i = 0; i < 5; i++) {
-            asyncService.asyncDefault2();
-        }
-        return "ok";
-    }
-
-    @GetMapping("/async-thread-pool")
-    public String asyncWithThreadPoolTaskExecutor() {
-        for (int i = 0; i < 5; i++) {
-            asyncService.asyncWithThreadPoolTaskExecutor();
-        }
-        return "ok";
-    }
-
-    @GetMapping("/async-no-bean-executor")
-    public String asyncNoBeanExecutor() {
-        for (int i = 0; i < 5; i++) {
-            asyncService.asyncNoBeanExecutor();
-        }
-        return "ok";
-    }
 }
